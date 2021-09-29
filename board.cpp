3d1c0cd37a1135f2067a920e375c8fff9789bcba
AGAPITO GALLART <agapito1998@gmail.com>
2021-09-29 18:58:43 +0200

    maze.cpp fully integrated

--- board.cpp
+++ board.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <vector>
+#include "./maze.cpp"
 
 using namespace std;
 
-typedef std::vector<std::vector<int>> Map;
-Map createMap(int,int); 
+typedef std::vector<std::vector<char>> Map;
+typedef std::vector<char> Row;
 
 class Board
 {
@@ -17,18 +18,34 @@ class Board
 
 Board::Board(int height, int width)
 {
-   this->map = createMap(height, width);
-}
+    Maze maze;
+    srand(time(0));
+    Row row = Row(width);
+    Map tmp;
+    for (int i = 0; i < height; ++i) tmp.push_back(row);
+    maze.maze(tmp);
+    maze.showMaze(tmp);
 
+}
 void Board::printBoard()
 {
-    for (size_t i = 0; i < Board::map.size(); i++)
-        for (size_t j = 0; j < Board::map[i].size(); j++)
-            std::cout << map[i][j] << ' ';
 }
 
-Map createMap(int m, int n)
+/*
+int main(int argc, char const *argv[])
 {
-    
+    Solution s;
+    int height = 6;
+    int width = 6;
+    srand(time(0));
+    vector<char> row(width);
+    vector<vector<char>> map;
+    for (int i = 0; i < height; ++i)
+    {
+        map.push_back(row);
+    }
+    s.maze(map);
+    s.showMaze(map);
+    return 0;
 }
-
+*/
--- maze.cpp
+++ maze.cpp
@@ -7,112 +7,101 @@
 #include <iostream>
 #include "algorithm"
 #include "stack"
+
 using namespace std;
 
-class Solution
+class Maze
 {
-public:
-    // Definition for singly-linked list.
-    void maze(vector<vector<char>> &map)
-    {
-        //U unvisited, ' ' visited
-        for (int i = 0; i < map.size(); ++i)
-        {
-            for (int j = 0; j < map[0].size(); ++j)
-            {
-                map[i][j] = '0';
-            }
-        }
-        _maze(map, 0, 0);
-    }
-
-    void showMaze(vector<vector<char>> &map)
-    {
-        for (int i = 0; i < map.size(); ++i)
-        {
-            for (int j = 0; j < map[0].size(); ++j)
-            {
-                cout << map[i][j];
-            }
-            cout << endl;
-        }
-    }
-    //Use DFS
-    void _maze(vector<vector<char>> &map, int i, int j)
-    {
-        int direct[][2] = {{0, 1}, {0, -1}, {-1, 0}, {1, 0}};
-        int visitOrder[] = {0, 1, 2, 3};
-        //out of boundary
-        if (i < 0 || j < 0 || i >= map.size() || j >= map[0].size())
-            return;
-#ifdef DEBUG
-        cout << "(" << i << ", " << j << ")" << endl;
-#endif
-        //visited, go back to the coming direction, return
-        if (map[i][j] == ' ')
-            return;
-
-        //some neightbors are visited in addition to the coming direction, return
-        //this is to avoid circles in maze
-        if (countVisitedNeighbor(map, i, j) > 1)
-            return;
-
-        map[i][j] = ' '; // visited
-
-        //shuffle the visitOrder
-        shuffle(visitOrder, 4);
-
-        for (int k = 0; k < 4; ++k)
-        {
-            int ni = i + direct[visitOrder[k]][0];
-            int nj = j + direct[visitOrder[k]][1];
-            _maze(map, ni, nj);
-        }
-    }
-
-    int countVisitedNeighbor(vector<vector<char>> &map, int i, int j)
-    {
-        int direct[][2] = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-        int count = 0;
-        for (int k = 0; k < 4; ++k)
-        {
-            int ni = i + direct[k][0];
-            int nj = j + direct[k][1];
-            //out of boundary
-            if (ni < 0 || nj < 0 || ni >= map.size() || nj >= map[0].size())
-                continue;
-            if (map[ni][nj] == ' ')
-                count++; //visited
-        }
-        return count;
-    }
-    void shuffle(int a[], int n)
-    {
-        for (int i = 0; i < n; ++i)
-        {
-            swap(a[i], a[rand() % n]);
-        }
-    }
-    void swap(int &a, int &b)
-    {
-        int c = a;
-        a = b;
-        b = c;
-    }
+    public:
+        void maze(vector<vector<char>> &map); // Definition for singly-linked list.
+        void showMaze(vector<vector<char>> &map);
+    private:
+        void _maze(vector<vector<char>> &map, int i, int j); //Use DFS
+        int countVisitedNeighbor(vector<vector<char>> &map, int i, int j);
+        void shuffle(int a[], int n);
+        void swap(int &a, int &b);
 };
-int main(int argc, char const *argv[])
+
+void Maze::maze(vector<vector<char>> &map)
 {
-    Solution s;
-    int height = 6;
-    int width = 6;
-    srand(time(0));
-    vector<char> row(width);
-    vector<vector<char>> map;
-    for (int i = 0; i < height; ++i)
+    //U unvisited, ' ' visited
+    for (int i = 0; i < map.size(); ++i)
     {
-        map.push_back(row);
+        for (int j = 0; j < map[0].size(); ++j)
+        {
+            map[i][j] = 'X';
+        }
     }
-    s.maze(map);
-    s.showMaze(map);
-    return 0;
+    _maze(map, 0, 0);
+}
+void Maze::showMaze(vector<vector<char>> &map)
+{
+    for (int i = 0; i < map.size(); ++i)
+    {
+        for (int j = 0; j < map[0].size(); ++j)
+        {
+            cout << map[i][j];
+        }
+        cout << endl;
+    }
+}
+void Maze::_maze(vector<vector<char>> &map, int i, int j)
+{
+    int direct[][2] = {{0, 1}, {0, -1}, {-1, 0}, {1, 0}};
+    int visitOrder[] = {0, 1, 2, 3};
+    //out of boundary
+    if (i < 0 || j < 0 || i >= map.size() || j >= map[0].size())
+        return;
+#ifdef DEBUG
+    cout << "(" << i << ", " << j << ")" << endl;
+#endif
+    //visited, go back to the coming direction, return
+    if (map[i][j] == ' ')
+        return;
+
+    //some neightbors are visited in addition to the coming direction, return
+    //this is to avoid circles in maze
+    if (countVisitedNeighbor(map, i, j) > 1)
+        return;
+
+    map[i][j] = ' '; // visited
+
+    //shuffle the visitOrder
+    shuffle(visitOrder, 4);
+
+    for (int k = 0; k < 4; ++k)
+    {
+        int ni = i + direct[visitOrder[k]][0];
+        int nj = j + direct[visitOrder[k]][1];
+        _maze(map, ni, nj);
+    }
+}
+int Maze::countVisitedNeighbor(vector<vector<char>> &map, int i, int j)
+{
+    int direct[][2] = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+    int count = 0;
+    for (int k = 0; k < 4; ++k)
+    {
+        int ni = i + direct[k][0];
+        int nj = j + direct[k][1];
+        //out of boundary
+        if (ni < 0 || nj < 0 || ni >= map.size() || nj >= map[0].size())
+            continue;
+        if (map[ni][nj] == ' ')
+            count++; //visited
+    }
+    return count;
+}
+void Maze::shuffle(int a[], int n)
+{
+    for (int i = 0; i < n; ++i)
+    {
+        swap(a[i], a[rand() % n]);
+    }
+}
+void Maze::swap(int &a, int &b)
+{
+    int c = a;
+    a = b;
+    b = c;
 }
