99f096d55b61aee625cb3eed994613c72b182eab
AGAPITO GALLART BERNAT <agapito1998@gmail.com>
2021-11-05 18:47:27 +0100

    opengl.h migration

--- opengl.h
+++ opengl.h
@@ -1,11 +1,11 @@
 #ifdef __linux__
-    #include <GL/glut.h>
+#include <GL/glut.h>
 #elif __APPLE__
-    #define GL_SILENCE_DEPRECATION
-    #include <OpenGL/glu.h>
-    #include <GLUT/glut.h>
+#define GL_SILENCE_DEPRECATION
+#include <GLUT/glut.h>
+#include <OpenGL/glu.h>
 #else
-    #error "OS not supported!"
+#error "OS not supported!"
 #endif
 
 #include <cmath>
@@ -17,11 +17,13 @@
 #define HEIGHT 300
 #define HEIGHT_WALL 20
 
-#define DISTANCE_UNIT 15          // 300/20 = 15
-#define DISTANCE_SUBUNIT 0.88235  // 15/17 = 0.88235...
-#define PI 3.14150
+#define DISTANCE_UNIT 15  // 300/20 = 15
+#define NUMBER_SUB_BLOCKS 16
+#define DISTANCE_SUB_UNIT 0.9375  // 15/16 = 0.9375...
+#define PI 3.141592653589793238462643383279502884197169
 
 #define MOVE 1
 #define QUIET 2
+#define ROTATE 3
 
 #pragma once
