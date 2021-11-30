b2ea27c4c694301a4c41a21df8b1bf4ac72ec657
AGAPITO GALLART BERNAT <agapito1998@gmail.com>
2021-12-01 00:49:18 +0100

    It's lights out on the way we go

--- game.cpp
+++ game.cpp
@@ -94,13 +94,13 @@ void display() {
 
     position[0] = 0;
     position[1] = 0;
-    position[2] = 0;
-    position[3] = 1;
+    position[2] = 60;
+    position[3] = 60;
     glLightfv(GL_LIGHT0, GL_POSITION, position);
 
-    color[0] = 0.1;
-    color[1] = 0.1;
-    color[2] = 0.1;
+    color[0] = 0.01;
+    color[1] = 0.01;
+    color[2] = 0.01;
     color[3] = 1;
     glLightfv(GL_LIGHT0, GL_AMBIENT, color);
     glEnable(GL_LIGHT0);
